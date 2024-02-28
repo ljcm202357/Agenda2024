3e52897c9cb34432d023598be442dd5e898d9435
unknown <APRENDIZ@BOGDAFCGFFSP278.FormacionSena.RED>
2024-02-28 12:23:25 -0500

    ingresar funciona

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,request,url_for
 import mysql.connector
 #creamos una instancia de la clase flask
 
@@ -18,22 +18,27 @@ cursor = db.cursor()
 def index():
     return render_template('index.html')
 
-@app.route('/Registrar', methods=['POST'])
+@app.route('/Registrar', methods=['GET', 'POST'])
 def registrar_usuario():
-    Nombres = request.form['nombre'],
-    Apellidos = request.form['apellido'],
-    Email = request.form['email'],
-    Direccion = request.form['direccion'],
-    Telefono = request.form['telefono'],
-    Usuario = request.form['usuario'],
-    Contrasena = request.form['contrasena']
+    if request.method == 'POST':
+       Nombres = request.form.get('nombre')
+       Apellidos = request.form.get('apellido')
+       Email = request.form.get('email')
+       Direccion = request.form.get('direccion')
+       Telefono = request.form.get('telefono')
+       Usuario = request.form.get('usuario')
+       Contrasena = request.form.get('contrasena')
 
     #insertar datos a la tabla personas
 
-    cursor.execute("INSERT INTO Personas(nombreper,apellidoper,emailper,dirper,telper,usuarioper,contraper)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombres,Apellidos,Email,Direccion,Telefono,Usuario,Contrasena))
-    db.commit()
+       cursor.execute("INSERT INTO Personas(nombreper,apellidoper,emailper,dirper,telper,usuarioper,contraper)VALUES(%s,%s,%s,%s,%s,%s,%s)",(Nombres,Apellidos,Email,Direccion,Telefono,Usuario,Contrasena))
+       db.commit()
 
-    return redirect(url_for('Registrar'))
+    # Redirigir a la misma página después de procesar la solicitud POST
+       return redirect(url_for('registrar_usuario'))
+
+    # Si la solicitud es GET, renderizar el formulario
+    return render_template('Registrar.html')
 
 #para ejecutar la aplicacion
 if __name__ == '__main__':
